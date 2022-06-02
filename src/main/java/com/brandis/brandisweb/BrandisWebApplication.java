88eeeb2c312fc01104c1ad0886a0697b9ea5e940
FGRed <nikoleppanen95@gmail.com>
2022-06-02 16:02:17 +0300

    Spring boot init

---
+++ src/main/java/com/brandis/brandisweb/BrandisWebApplication.java
@@ -0,0 +1,13 @@
+package com.brandis.brandisweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandisWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandisWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandis/brandisweb/BrandisWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandis.brandisweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandisWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
