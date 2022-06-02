f0d3940595b484bb8a91f261ce3ae11e0e4a54d7
Timo <Sumonen@yahoo.com>
2022-06-02 20:15:05 +0300

    Added BGame repository and model.

---
+++ src/main/java/com/brandis/brandisweb/repository/BGameRepository.java
@@ -0,0 +1,9 @@
+package com.brandis.brandisweb.repository;
+
+import com.brandis.brandisweb.model.BGame;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BGameRepository extends JpaRepository<BGame, Long> {
+}
