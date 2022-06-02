d532aeb0c97390c75b853285e7265236c0cb625e
FGRed <nikoleppanen95@gmail.com>
2022-06-02 21:09:25 +0300

    added BProduct

---
+++ src/main/java/com/brandis/brandisweb/repository/BProductRepository.java
@@ -0,0 +1,9 @@
+package com.brandis.brandisweb.repository;
+
+import com.brandis.brandisweb.model.BProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BProductRepository extends JpaRepository<BProduct, Long> {
+}
