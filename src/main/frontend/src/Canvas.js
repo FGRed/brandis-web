facc6ee88f10891a32c1cce08863ecfa8649fac7
Timo <Sumonen@yahoo.com>
2022-06-15 18:48:36 +0300

    Added functionality to Menu button so that it opens a side canvas.

---
+++ src/main/frontend/src/Canvas.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function Canvas(){
+     return(
+    <div>
+
+    </div>
+    )
+}
+
+export default Canvas
--- src/main/frontend/src/Menu.js
+++ src/main/frontend/src/Menu.js
@@ -1,10 +1,31 @@
 import React from "react";
+import Canvas from "./Canvas";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import {useState} from 'react';
 
-class Menu extends React.Component{
-    render() { return(
-        <button className="btn btn-outline-primary btn-lg my-2 mx-3"><i className="fa fa-solid fa-bars"/></button>
-    )
-    }
+function Menu(){
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+     return(
+
+        <div>
+        <button className="btn btn-outline-primary btn-lg my-2 mx-3" onClick={handleShow}>
+            <i className="fa fa-solid fa-bars"/>
+        </button>
+        <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                Some text as placeholder. In real life you can have the elements you
+                have chosen. Like, text, images, lists, etc.
+            </Offcanvas.Body>
+        </Offcanvas>
+        </div>
+    );
 }
 
 export default Menu
