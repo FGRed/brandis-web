f1d466ae4afd376d8922ba982207a345c8ad949f
Timo <Sumonen@yahoo.com>
2022-06-23 17:56:48 +0300

    Added marketing component.

--- src/main/frontend/src/Game.js
+++ src/main/frontend/src/Game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MerchantComponent from "./MerchantComponent";
+import MarketingComponent from "./MarketingComponent";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
@@ -9,6 +10,7 @@ export default function Game(){
             <Row className="shadow-sm px-2 py-3 my-3 mx-1 border">
                 <Col><MerchantComponent/></Col>
                 <Col><MerchantComponent/></Col>
+                <Col><MarketingComponent/></Col>
             </Row>
         </>
     )
